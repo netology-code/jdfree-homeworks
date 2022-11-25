045b9e64bb43e4d19b3cf0c0650dcad7f4071822
MihailOkatev <mokatev@yandex.ru>
2022-11-25 15:17:25 +0300

    Upd 4 hw

---
+++ 04/src/main/java/Main.java
@@ -0,0 +1,8 @@
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+public class Main {
+
+    public static void main(String[] args) throws TelegramApiException {
+        new MyTelegramBot();
+    }
+}
---
+++ 04/src/main/java/MyTelegramBot.java
@@ -0,0 +1,68 @@
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
+import java.io.IOException;
+
+public class MyTelegramBot extends TelegramLongPollingBot {
+    public static final String BOT_TOKEN = "ВАШ ТОКЕН";
+
+    public static final String BOT_USERNAME = "ВАШ БОТ";
+
+    public static final String URI = "https://api.nasa.gov/planetary/apod?api_key=ВАШ КЛЮЧ";
+
+    public static long chatId;
+
+    public MyTelegramBot() throws TelegramApiException {
+        TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
+        try {
+            botsApi.registerBot(this);
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public String getBotUsername() {
+        return BOT_USERNAME;
+    }
+
+    @Override
+    public String getBotToken() {
+        return BOT_TOKEN;
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        if (update.hasMessage()) {
+            chatId = update.getMessage().getChatId();
+            switch (update.getMessage().getText()) {
+                case "/help":
+                    sendMessage("Привет, я бот NASA! Я высылаю ссылки на картинки по запросу. " +
+                            "Напоминаю, что картинки на сайте NASA обновляются раз в сутки");
+                    break;
+                case "/give":
+                    try {
+                        sendMessage(Utils.getUrl(URI));
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                    break;
+            }
+        }
+    }
+
+    private void sendMessage(String messageText) {
+        SendMessage message = new SendMessage();
+        message.setChatId(chatId);
+        message.setText(messageText);
+        try {
+            execute(message);
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+}
